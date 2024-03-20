93e21890b3b136862a883be20cb8e2ab50df278e
Julio Cesar Pedroso <juliocp@weg.net>
2024-03-20 14:15:20 -0300

    MovieFilter container styling

---
+++ src/components/MovieFilter/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+const MovieFilter = () => {
+    return (
+        <div className="base-card movie-filter-container">
+            <div className="movie-filter-genre-container">
+            <select >
+                <option value="">Aventura</option>                
+            </select>
+            </div>
+        </div >
+    );
+}
+
+export default MovieFilter;
--- src/pages/Private/MovieCatalog/index.tsx
+++ src/pages/Private/MovieCatalog/index.tsx
@@ -1,30 +1,28 @@
 import MovieCard from "components/MovieCard";
+import MovieFilter from "components/MovieFilter";
 import { Link } from "react-router-dom";
 
 const MovieCatalog = () => {
     return (
         <div className="container my-4 catalog-container">
-            <div className="row catalog-title-container">
-                <h1>Tela listagem de filmes</h1>
-            </div>
-
-            <MovieCard/>
-            <MovieCard/>
-            <MovieCard/>
-
-
-            <div className="row my-4">
-
-                <Link className="text-white" to="/movies/1">
-                    Acessar /movies/1
-                </Link>
+            <div className="movie-filter-bar-container">
+                <MovieFilter />
             </div>
             <div className="row">
-
-                <Link className="text-white" to="/movies/2">
-                    Acessar /movies/2
-                </Link>
+                <div className="col-sm-6 col-md-12">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-md-12">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-md-12">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-md-12">
+                    <MovieCard />
+                </div>
             </div>
+
         </div>
     )
 }
